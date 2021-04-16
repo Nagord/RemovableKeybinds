eea31a83b51ee344efb67d218a54ebff7a3a59f5
DragonFire47 <1redyoung@gmail.com>
2021-04-16 18:34:43 -0400

    Add project files.

---
+++ RemovableKeybinds/Patch.cs
@@ -0,0 +1,19 @@
+﻿using HarmonyLib;
+using UnityEngine;
+
+namespace RemovableKeybinds
+{
+    [HarmonyPatch(typeof(PLUIControlsMenu), "KeyCodeHandler")]
+    class Patch
+    {
+        static void Prefix(ref KeyCode key)
+        {
+            PulsarPluginLoader.Utilities.Logger.Info("Running prefix");
+            if(key == KeyCode.Escape)
+            {
+                PulsarPluginLoader.Utilities.Logger.Info("Matched check");
+                key = KeyCode.None;
+            }
+        }
+    }
+}
---
+++ RemovableKeybinds/Plugin.cs
@@ -0,0 +1,20 @@
+﻿using PulsarPluginLoader;
+
+namespace RemovableKeybinds
+{
+    public class Plugin : PulsarPlugin
+    {
+        public override string Version => "1.0.0";
+
+        public override string Author => "Dragon";
+
+        public override string LongDescription => "Removes keybind if escape is typed";
+
+        public override string Name => "RemovableKeybinds";
+
+        public override string HarmonyIdentifier()
+        {
+            return "Dragon.RemovableKeybinds";
+        }
+    }
+}
